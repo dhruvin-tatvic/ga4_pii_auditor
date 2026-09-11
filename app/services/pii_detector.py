1c2ef16d980235f955f9cc33743bab8b3a2dd33f
dhruvin-tatvic <dhruvin@tatvic.com>
2026-09-11 14:53:11 +0530

    Regex updated

--- app/services/pii_detector.py
+++ app/services/pii_detector.py
@@ -2,9 +2,11 @@ import re
 
 class PIIDetector:
     def __init__(self):
-        self.email_regex = re.compile(r'[a-zA-Z0-9_.+-]+(?:@|%40)[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+', flags=re.IGNORECASE)
-        self.phone_regex = re.compile(r'\b(?:(?:\+|%2B)(?:1|91)[-.\s%20])?\(?[2-9]\d{2}\)?[-.\s%20]+[2-9]\d{2}[-.\s%20]+\d{4}\b|\b(?:\+|%2B)(?:1|91)[-.\s%20][2-9]\d{9}\b')
-        self.name_regex = re.compile(r'(?:\b||-)(?:first_?name|last_?name|fname|lname|name)=([^&#\s]+)', flags=re.IGNORECASE)
+        self.email_regex = re.compile(r'\b[A-Za-z0-9][A-Za-z0-9._%+-]*?(?:@|%40)[A-Za-z0-9-]+(?:\.[A-Za-z0-9-]+)+\b',flags=re.IGNORECASE)
+
+        self.phone_regex = re.compile(r'(?<!\d)(?:(?:\+|%2B)(?:91|1)[-.\s%20]?)?(?:[6-9]\d{9})(?!\d)',flags=re.IGNORECASE)
+
+        self.name_regex = re.compile(r'(?:^|[?&])(?:first_?name|last_?name|fname|lname|full_?name|customer_?name|user_?name)=([^&#\s]+)',flags=re.IGNORECASE)
 
     def scan_value(self, value):
         try:
